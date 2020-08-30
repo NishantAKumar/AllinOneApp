c2c6e2f7b631507446b8f4e6450a08a5d569e2c5
NishantAKumar <63119114+NishantAKumar@users.noreply.github.com>
2020-08-30 18:25:57 +0530

    Updated

--- Scheduler/urls.py
+++ Scheduler/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('create', views.create, name = 'create'),
     path('read', views.read, name = 'read'),
     path('update', views.update, name = 'update'),
-    path('delete', views.delete, name = 'delete')
+    path('delete', views.delete, name = 'delete'),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('profile', views.profile, name='profile'),
+    path('logout', views.logout, name='logout')
 ]
--- Scheduler/views.py
+++ Scheduler/views.py
@@ -1,11 +1,53 @@
 from django.shortcuts import render, redirect
 from django.http.response import HttpResponse
 from django.contrib import messages
+from django.contrib.auth.models import auth
+from .models import MyAccountManager, User
 # login function yet to be included
 # db model yet to be created
 events = []
 
+def logout(request):
+    auth.logout(request)
+    return redirect ("/")
 
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'Scheduler/login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/")
+        else:
+            messages.info(request, "Invalid Credentials")
+            return redirect("/login")
+
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'Scheduler/register.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm = request.POST.get('confirmation')
+
+        if password == confirm:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('/register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('/register')
+            else:
+                user = User.objects.create_user(password=password, email=email, username=username)
+                return redirect('/login')
+        else:
+            messages.info(request, 'Passwords Do Not Match')
+            return redirect('/register')
+        
 def options(request):
     return render(request, 'Scheduler/options.html')
 
@@ -43,3 +85,6 @@ def delete(request):
                 events.remove(i)
                 messages.info(request, 'Event Deleted')
         return redirect('delete')
+
+def profile(request):
+    return render(request, 'Scheduler/profile.html')
